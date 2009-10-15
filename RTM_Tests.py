d24deb9d099ace48ec609bcd31183e8abb5725ec
Gordon Fontenot <gordon.fontenot@gmail.com>
2009-10-15 09:32:12 -0400

    Passes parsed XML to variable succesfully

--- RTM_Tests.py
+++ RTM_Tests.py
@@ -10,7 +10,7 @@ import webbrowser
 #import ElementTree to parse / work with XML
 from xml.etree import ElementTree as ET
 
-#import os to work with paths
+#import os to work with paths.  May not need this.
 import os
 
 def main():
@@ -21,22 +21,31 @@ def main():
 	api_key='60a9369798aa92cc5cc291b2280422f1'
 	api_secret='6fdf8ca0e501715f'
 	
-	# Get the location of the response XML file
+	# Get the location of the response XML file.  May not need this.
 	xml_resp = os.path.abspath(__file__)
 	xml_resp = os.path.dirname(xml_resp)
 	xml_resp = os.path.join(xml_resp, "resp.xml")
 	
 	def callURL(method):
-		url=api_url+'method='+method+'&api_key='+api_key+'&test=chili'
+		url=api_url+'method='+method+'&api_key='+api_key+'&test=chupacabra'
 
 		page = urllib.urlopen(url)
-		the_resp=ET.parse(page)
-		tree=the_resp.getroot()
+		#the_resp=ET.parse(page)
+		#tree=the_resp.getroot()
 		
-		for element in tree:
+		the_resp=ET.parse(page).getroot()
+		
+		
+		for element in the_resp:
 			print element.text
 			
-		the_resp.write(xml_resp)
+		for element in the_resp.findall('api_key/'):
+			var = element.text
+		print
+		print var
+		
+		#Write the response to the local XML file.  May not need this	
+		#the_resp.write(xml_resp)
 		
 		pass
 		
