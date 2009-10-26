bbbde7acfa2bb29ec8b174b7fa2ece73778e8799
Gordon Fontenot <gordon.fontenot@gmail.com>
2009-10-26 11:18:12 -0400

    Cleaned up code, commented out dev messages

--- RTM_Tests.py
+++ RTM_Tests.py
@@ -41,14 +41,13 @@ def main():
 	api_secret='6fdf8ca0e501715f'
 	the_plist='~/Library/Preferences/com.rememberthemilk.RTM-QSB.plist'
 	the_plist=NSString.stringByExpandingTildeInPath(the_plist)
-	m=hashlib.md5()
 	auth=1
 	the_token=0
 	
 	# Get the location of the response XML file.  Used for dev only.
-	xml_resp = os.path.abspath(__file__)
-	xml_resp = os.path.dirname(xml_resp)
-	xml_resp = os.path.join(xml_resp, 'resp.xml')
+	#xml_resp = os.path.abspath(__file__)
+	#xml_resp = os.path.dirname(xml_resp)
+	#xml_resp = os.path.join(xml_resp, 'resp.xml')
 	
 	
 	def getLocalToken():
@@ -56,52 +55,43 @@ def main():
 			mydict = NSDictionary.dictionaryWithContentsOfFile_(the_plist)
 			the_token = mydict['Token']
 			return the_token
-			pass
 		else:
-			print 'No Token Found'
+			#print 'No Token Found'
 			return 0
 	
 	#Check to see if the Token is valid
 	def checkToken(the_token):
 		method = 'rtm.auth.checkToken'
 		
-		url=api_url+'method='+method+'&api_key='+api_key+'&auth_token='+the_token
-		
-		the_token = ParseURL(url, 'token/')
+		url = "%smethod=%s&api_key=%s&auth_token=%s" % (api_url, method, api_key, the_token)
+		the_token = ParseURL(url, 'auth/token/')
 		return the_token
-		
-		pass
 	
 	#Function to write to the plist.  Only sets the token for now.  Could add in more parameters later.
 	def writePlist(the_token):
 		mydict = {}
 		mydict['Token']=the_token
 		NSDictionary.dictionaryWithDictionary_(mydict).writeToFile_atomically_(the_plist, True)
-		pass
 
 	
 	#get the Frob to begin auth process, because the token came back false
 	def getFrob():
 		method = 'rtm.auth.getFrob'
 		
-		the_frob=0
-		the_sig = api_secret+'api_key'+api_key+'method'+method
-
+		the_sig = "%sapi_key%smethod%s" % (api_secret, api_key, method)
 		hashed_sig= createMD5(the_sig)
 		
-		url=api_url+'method='+method+'&api_key='+api_key+'&api_sig='+(str(hashed_sig))
+		url = "%smethod=%s&api_key=%s&api_sig=%s" % (api_url, method, api_key, hashed_sig)
 		
 		the_frob=ParseURL(url, 'frob/')
+		
 		return the_frob
 		
 		pass
 	
 	
 	def createMD5(the_string):
-		m.update(the_string)
-		the_hash = m.hexdigest()
-		return the_hash
-		pass
+		return hashlib.md5(the_string).hexdigest()
 	
 	def doAuth():
 		#Need to get a frob to be used to obtain a new token
@@ -110,137 +100,148 @@ def main():
 		#Get the user to give their auth via the RTM website
 		getAuth(the_frob)
 		
-		# #sleep for 30 seconds to allow user to grant auth before proceeding with getting the token.  This needs to be implimented better.
-		# 		time.sleep(30)
-		# 		#Should have auth by now.  May run into problems where user isn't paying attention, didn't auth.  Again, there may be a better solution for this.
-		# 		
-		# 		#Start to get the actual token that will be stored in our plist.
-		# 		the_token=getRemoteToken(the_frob)
-		# 		
-		# 		if the_token != 0:
-		# 			#Token came back successfully.  Display success message for Dev
-		# 			
-		# 			print 'Sucess'
-		# 			print 'Token: '+the_token
-		# 			
-		# 			#Store token in plist
-		# 			#writePlist(the_token)
-		# 			return 1
-		# 		else:
-		# 			#Token did not come back succesfully Should maybe add some error handling here
-		# 			
-		# 			print 'Failure'
-		# 			return 0
+		#sleep for 30 seconds to allow user to grant auth before proceeding with getting the token.  This needs to be implimented better.
+		time.sleep(15)
+		#Should have auth by now.  May run into problems where user isn't paying attention, didn't auth.  Again, there may be a better solution for this.
+		
+		#Start to get the actual token that will be stored in our plist.
+		the_token=getRemoteToken(the_frob)
+		
+		if the_token != 0:
+			#Token came back successfully.  Display success message for Dev
+			
+			#print 'Sucess'
+			#print 'Token: '+the_token
+			
+			#Store token in plist
+			writePlist(the_token)
+			return 1
+		else:
+			#Token did not come back succesfully Should maybe add some error handling here
+			
+			#print 'Failure'
+			return 0
 		pass
 	
 	def getRemoteToken(the_frob):
 		method = 'rtm.auth.getToken'
 
-		the_sig = api_secret+'api_key'+api_key+'frob'+the_frob+'method'+method
-		
+		the_sig = "%sapi_key%sfrob%smethod%s" % (api_secret, api_key, the_frob, method)
 		hashed_sig= createMD5(the_sig)
 		
-		url=api_url+'method='+method+'&api_key='+api_key+'&frob='+the_frob+'&api_sig='+(str(hashed_sig))
+		url = "%smethod=%s&api_key=%s&frob=%s&api_sig=%s" % (api_url, method, api_key, the_frob, hashed_sig)
 		
-		the_token = ParseURL(url, 'token/')
+		the_token = ParseURL(url, 'auth/token/')
 		return the_token
-		pass
-	
+			
 	def getAuth(the_frob):
 		
 		method = 'rtm.auth.getFrob'
-		the_sig = api_secret+'api_key'+api_key+'frob'+the_frob+'permswrite'
+		
+		the_sig = "%sapi_key%sfrob%sthe_frob%spermswrite" % (api_secret, api_key, the_frob)
 		hashed_sig= createMD5(the_sig)
-		url=auth_url+'api_key='+api_key+'&frob='+the_frob+'&perms=write&api_sig='+(str(hashed_sig))
+		
+		url = "%sapi_key=%s&frob=%s&perms=write&api_sig=%s" % (auth_url, api_key, the_frob, hashed_sig)
 		
 		webbrowser.open(url)
 		
 		# For dev: Print out the full URL opened, for error checking.
-		print 'Website Opened:'
-		print url
-		
-		pass
+		#print 'Website Opened:'
+		#print url
 	
-	def SendTask(the_token, new_task):
+	def SendTask(new_task):
+		
+		#get the local token (again.  Was having problems with the token not being recognized as global.  This solves the issue)
+		the_token=getLocalToken()
+		
 		#need to create timeline.
-		timeline=createTimeline()
+		timeline=createTimeline(the_token)
 		
 		method ='rtm.tasks.add'
 		
 		#sets the parse value to 1.  With it set to 1, smart-add is in effect.
 		doParse = '1'
 		
-		the_sig=api_secret+'api_key'+api_key+'method'+method+'name'+new_task+'parse'+doParse+'timeline'+timeline
-		hashed_sig=creadeMD5(the_sig)
+		the_sig = "%sapi_key%sauth_token%smethod%sname%sparse%stimeline%s" % (api_secret, api_key, the_token, method, new_task, doParse, timeline)
+		hashed_sig=createMD5(the_sig)
 		
-		url = api_url+'method'+method+'&api_key='+api_key+'&timeline='+timeline+'&name='+new_task+'&parse='+doParse+'&api_sig='+(str(hashed_sig))		
-		
+		new_task=makeNetSafe(new_task)
+				
+		url = "%smethod=%s&api_key=%s&timeline=%s&name=%s&parse=%s&auth_token=%s&api_sig=%s" % (api_url, method, api_key, timeline, new_task, doParse, the_token, hashed_sig)
 		ParseURL(url, 0)
-		return var
-		pass
 
 	#function to create timeline for sendTask function
-	def createTimeline():
+	def createTimeline(the_token):
 		method='rtm.timelines.create'
 		
-		url=api_url+'method='+method+'&api_key='+api_key
+		the_sig = "%sapi_key%sauth_token%smethod%s" % (api_secret, api_key, the_token, method)
+		hashed_sig=createMD5(the_sig)
+		
+		url = "%smethod=%s&api_key=%s&auth_token=%s&api_sig=%s" % (api_url, method, api_key, the_token, hashed_sig)
 		
 		#send url to the parser
 		timeline = ParseURL(url, 'timeline/')
 		return timeline
-		pass
 	
 	#Function to call and parse the URL.
 	def ParseURL(url, ItemNeeded):
 		
 		page = urllib.urlopen(url)
-
+		#print "Url sent: " + url
 		#Seperate the variable from the file. Used to write the Resp to disk. Used for development. May not need this.
-		the_resp=ET.parse(page)
-		tree=the_resp.getroot()
+		#the_resp=ET.parse(page)
+		#tree=the_resp.getroot()
 
 		#Parse the XML
-		#the_resp=ET.parse(page).getroot()
+		the_resp=ET.parse(page).getroot()
 		
 		var = 0	
 		if ItemNeeded !=0:
 			#essentially, is this from the sendtask method.  This sucks.  Figure out a better way to do this.
 			
 			#Grab the response message	
-			for element in tree.findall(ItemNeeded):
-				var = element.text
+			for element in the_resp.findall(ItemNeeded):
+				var = str(element.text)
 
 		#Write the response to the local XML file. Used for Dev only.	
-		the_resp.write(xml_resp)
+		#the_resp.write(xml_resp)
 
 		return var
+	
+	def makeNetSafe(new_task):
+		new_task=new_task.split()
+		return "%20".join(new_task)
 		pass
 		
-	#Read the plist, grab the Token (using test string for dev)
-	the_token=getLocalToken()
-	the_token=(str(the_token))
 	
-	if the_token != 0:
+	#------------------------------------------------#
+	#-----------END OF FUNCTIONS---------------------#
+	#-------BEGIN MAIN FUNCTION----------------------#
+	#------------------------------------------------#
+	
+	#Read the plist, grab the Token (using test string for dev)
+	local_token=getLocalToken()
+	
+	if local_token != 0:
 		#There is a token, need to check to make sure it isn't expired.
 		
 		#Check token't validity
-		result = checkToken(the_token)
+		result = checkToken(local_token)
 		if result == 0:
 			# Token came back false, token is expired.
 			auth=doAuth()
 	else:
 		#There is no token. We need to run through the auth process and save a new plist
-		auth=doAuth(the_frob)
+		auth=doAuth()
 
 	if auth == 1:
 		#Auth was sucessfull, should have a token to use.
 		
 		#Call SendTask function to create new task
-		#SendTask(the_token, the_task)
-		pass
-	else:
+		SendTask(the_task)
+	#else:
 		#something went wrong. Print a failure message for dev.
-		print 'An error occured during the code.  Auth not sucessfull.'
+		#print 'An error occured during the code.  Auth not sucessfull.'
 		
 		
 if __name__ == '__main__':
